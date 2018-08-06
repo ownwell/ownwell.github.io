3b0777ce8340181037811cc6b36ff72378bbfddc
Cyning <ownwell@126.com>
2018-08-06 22:35:23 +0800

    Site updated: 2018-08-06 22:35:22

--- js/totop.js
+++ js/totop.js
@@ -1,35 +1,29 @@
-function myEvent(obj,ev,fn){
-	if(obj.attachEvent){
-		obj.attachEvent('on'+ev,fn);
-	}else{
-		obj.addEventListener(ev,fn,false);
-	}
-}
-myEvent(window,'load',function(){
-	var oRTT=document.getElementById('back_to_top');
-	var pH=document.documentElement.clientHeight;
-	var timer=null;
-	var scrollTop;
-	window.onscroll=function(){
-		scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
-		if(scrollTop>=pH){
-			oRTT.style.display='block';
-		}else{
-			oRTT.style.display='none';
-		}
-		return scrollTop;
-	};
-	oRTT.onclick=function(){
-		clearInterval(timer);
-		timer=setInterval(function(){
-			var now=scrollTop;
-			var speed=(0-now)/10;
-			speed=speed>0?Math.ceil(speed):Math.floor(speed);
-			if(scrollTop==0){
-				clearInterval(timer);
-			}
-			document.documentElement.scrollTop=scrollTop+speed;
-			document.body.scrollTop=scrollTop+speed;
-		}, 30);
-	}
-});
+(function($) {
+    
+    // When to show the scroll link
+    // higher number = scroll link appears further down the page    
+    var upperLimit = 200; 
+        
+    // Our scroll link element
+    var scrollElem = $('#totop');
+    
+    // Scroll to top speed
+    var scrollSpeed = 500;
+    
+    // Show and hide the scroll to top link based on scroll position    
+    scrollElem.hide();
+    $(window).scroll(function () {             
+        var scrollTop = $(document).scrollTop();        
+        if ( scrollTop > upperLimit ) {
+            $(scrollElem).stop().fadeTo(300, 1); // fade back in            
+        }else{        
+            $(scrollElem).stop().fadeTo(300, 0); // fade out
+        }
+    });
+
+    // Scroll to top animation on click
+    $(scrollElem).click(function(){ 
+        $('html, body').animate({scrollTop:0}, scrollSpeed); return false; 
+    });
+
+})(jQuery);
